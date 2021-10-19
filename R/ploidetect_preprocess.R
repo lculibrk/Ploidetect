e5a758e41be3cb496a78f9dedfadcff0d2571078
lculibrk <luculibrk@gmail.com>
2021-10-19 12:34:59 -0700

    Tidied up ploidetect_preprocess.R

--- R/ploidetect_preprocess.R
+++ R/ploidetect_preprocess.R
@@ -7,9 +7,6 @@ ploidetect_preprocess <- function(all_data, centromeres = F, debugPlots = F, ver
   # Load data
   x <- as.data.frame(all_data)
   
-
-    
-  
   # Test if data is configured and input properly
   if(any(grepl("chr", x$chr))){
     stop("Expected numeric chromosomes (1, 2, 3, ... X), not chr1, chr2, etc")
@@ -20,9 +17,6 @@ ploidetect_preprocess <- function(all_data, centromeres = F, debugPlots = F, ver
   if(!all(is.numeric(x$tumour))){
     stop("At least one element in tumour column is not numeric.")
   }
-  #if(!all(is.numeric(x$maf) | is.na(x$maf))){
-  #  stop("VAF column must contain only numeric or NA values")
-  #}
   if(!all(is.numeric(x$gc))){
     stop("At least one element in GC-content column is not numeric")
   }
@@ -30,8 +24,7 @@ ploidetect_preprocess <- function(all_data, centromeres = F, debugPlots = F, ver
   ## Step 1: Merge data such that window size is about 100Kb
   
   ## Filter for chr1-22 and X
-  
-  #x <- x[grepl(pattern = paste0(c(paste0("^", 1:22), "^X"), "_", collapse = "|"), x = x[,window_id]),]
+
   
   if(verbose){
     print("Filtering for chromosomes 1-22 and X")
@@ -46,40 +39,39 @@ ploidetect_preprocess <- function(all_data, centromeres = F, debugPlots = F, ver
     if(verbose){
       print("Filtering out centromeric loci")
     }
+    ## Split by chromosome
     x <- split(x, x$chr)
+    ## Strip "chr" from chromosomes if present
     if(grepl("chr", centromeres$chr[1])){
       centromeres[,chr:=gsub("chr", "", chr)]
     }
+    ## Not entirely sure why this is here, 
+    ## TODO: Test this
     centromeres_preprocess <- centromeres %>% group_by(chr) %>% dplyr::summarise(pos = first(pos), end = last(end))
+    ## Split centromeres by chromosome
     centromeres_split <- split(centromeres_preprocess, centromeres_preprocess$chr)
+    ## Loop by chromosome, filter out centromeric positions
     x <- lapply(x, function(k){
       chr <- k$chr[1]
-      print(chr)
-      print(centromeres_split[[chr]])
-      centro_start <- centromeres_split[[chr]]$pos %>% unlist()
-      centro_end <- centromeres_split[[chr]]$end %>% unlist()
-
-      #print(str(k))
+      centro_start <- unlist(centromeres_split[[chr]]$pos)
+      centro_end <- unlist(centromeres_split[[chr]]$end)
       k <- k %>% filter(end < centro_start | pos > centro_end)
-      #print(str(k))
       return(k)
     })
+    ## Merge data back from list form
     x <- rbindlist(x) %>% arrange(chr, pos)
     if(verbose){
       print("Completed centromere filtering")
     }
   }
 
-  
+  ## Get mean bin size
   x$window_size <- x$end - x$pos
-  
-  
-  #mean_size <- mean(x[,window_size])
   mean_size <- mean(x$window_size)
-  
+  ## Figure out how many times we need to merge bins to create low-resolution TC/Ploidy bins
   closest <- round(simplify_size/mean_size, digits = 0)
   if(simplify){
-    # Compute the closest integer multiple of the window size to 100kb
+    # Compute the closest integer multiple of the window size to the simplify size
     if(closest < 1){
       closest = 1
     }
@@ -87,23 +79,9 @@ ploidetect_preprocess <- function(all_data, centromeres = F, debugPlots = F, ver
     x$merge <- floor(seq(from = 0, by = 1/closest, length.out = nrow(x)))
   }else{x$merge <- 1:nrow(x)}
   
-  # Process the allele frequency column into a numeric vector
-  
-  #x[,avg_allele_freq][which(x[,avg_allele_freq] == ".")] <- NA
+  # Convert "." in baf column to NA
   x$maf[which(x$maf == ".")] <- NA
-  
-  
-  #x[,avg_allele_freq] <- as.numeric(x[,avg_allele_freq])
-  #x$maf <- as.numeric(x$maf)
-  
-  #x$chr <- gsub("_.*", "", x[,window_id])
-  
-  # Sanitize the column names
-  
-  #x <- x[,c(tumour, normal, avg_allele_freq, window_id, window_size, GC, 7, 8)]
-  
-  #names(x) <- c("tumour", "normal", "maf", "wind", "size", "gc", "merge", "chr")
-  #x <- x %>% group_by(merge, chr) %>% summarise(tumour = sum(tumour), normal = sum(normal), maf = merge_mafs(maf, na.rm = T), wind = dplyr::first(wind), size = sum(size), gc = mean(gc))
+  ## Merge based on the aggregation column
   if(simplify){
     x <- data.table(x)
     x <- x[,.(pos = first(pos), end = last(end), tumour = sum(tumour), normal = sum(normal), maf = merge_mafs(maf, na.rm = T, exp = T), gc = mean(gc), window_size = sum(window_size)), by = list(chr, merge)]
@@ -115,48 +93,33 @@ ploidetect_preprocess <- function(all_data, centromeres = F, debugPlots = F, ver
   d <- density(x$tumour, n = nrow(x))
   maxpeak <- d$x[which.max(d$y)]
   
+  ## Record the bins used to compute the maxpeak to use later
   maxpeak_ind <- which(x$tumour < (maxpeak + d$bw) & x$tumour > (maxpeak - d$bw))
 
   ## Remove tibble formatting
   x <- as.data.frame(x)
   
-  ## Set row names to window_ids
-  #row.names(x) <- x$wind
 
   ## Get median normal coverage
   median_normal <- median(x$normal)
   
+  ## Compute normalization factors by comparing the individual
   norm_factor <- x$normal/median_normal
   
+  ## Correct tumour depth by the norm factor
   x$tumour <- x$tumour/norm_factor
   
-
-  ## Perform basic pre-filtering, find the windows within the 90th percentile of tumour read counts
-  #rangedf <- x[findInterval(x$tumour, 
-  #                          quantile(x$tumour, 
-  #                                   probs=c(0,0.90))) == 1,]
-  
-  ## Obtain the range of values observed within the 90th percentile of tumour read counts
-  #range <- range(rangedf$tumour)[2] - range(rangedf$tumour)[1]
-  
-  ## Maximum allowable read depth for retention is the 90th percentile + the above computed range.
-  #max <- range(rangedf$tumour)[2] + range
-  #min <- 0
-  
-  ## Set outliers aside for later steps (these will be CNA called later, but are exempt from TC/Ploidy analysis)
-  #highoutliers <- x[findInterval(x$tumour, c(min, max)) > 1,]
-  
-  ## Filter data for everything within the read depth range
-  #x <- x[findInterval(x$tumour, c(min, max)) == 1,]
-  
   # Extract X chromosome regions
-  
+  ## TODO: Make this compatible with running without X-chromosomes
+  ## Filter X chromosome data
   chrX <- x[x$chr == "X",]
+  ## Decide whether this is a male or not
   isMale=F
   if(which.min(abs((median(chrX$window_size) - median(x$window_size)) - c(0, median(x$window_size)))) == 2){
     x$normal[x$chr == "X"] <- x$normal[x$chr == "X"]/2
     isMale=T
   }
+  ## Print sex result
   if(verbose){
     if(isMale){
       print("Automated sex detection infers MALE sex")
@@ -165,18 +128,11 @@ ploidetect_preprocess <- function(all_data, centromeres = F, debugPlots = F, ver
     }
   }
 
-  ## This is a very broad-strokes filtering step for window size. Basically removing extreme outliers w.r.t germline mappability, as we don't want to use these in modeling
-  #x <- x[(x$window_size > (median(x$window_size)/10)) & (x$window_size < (median(x$window_size)*5)),]
-  
-  
-  #x <- x[,c(tumour, normal, window_id, avg_allele_freq, window_size, GC)]
-  #names(x) <- c("y_raw", "x_raw", "window", "maf", "size", "GC")
-  
-  #x <- x[,3:8]
-  
-  #names(x) <- c("y_raw", "x_raw", "maf", "window", "size", "GC")
+  ## Rename columns to tumour/normal
   x <- x %>% dplyr::rename("y_raw" = "tumour", "x_raw" = "normal")
   
+  ## Debugging plots, hardly used anymore anyways
+  ## Shows the data normalization progress
   plot_limits_size <- quantile(x$window_size, probs = c(0.005, 0.995))
   plot_limits_depth <- quantile(x$y_raw, probs = c(0.99))
   plot_limits_depth <- c(0, plot_limits_depth)
@@ -189,10 +145,6 @@ ploidetect_preprocess <- function(all_data, centromeres = F, debugPlots = F, ver
         axis.text = element_text(size = 15)
       ) + scale_x_continuous(limits = plot_limits_size) + scale_y_continuous(limits = plot_limits_depth)
     print(rawPlot)
-    
-    #x %>% filter(chr == 10) %>% ggplot(aes(x = pos, y = y_raw)) + geom_point(size = 0.5) + theme_bw() + ylab("Raw depth") + xlab("Position") + ggtitle("Uncorrected Read Depth")
-    
-    
   }
   if(debugPlots){
     GCplot <- ggplot(x, aes(x = gc * 100, y = y_raw)) + geom_point(size = 0.3, alpha = 0.2) + theme_bw() + xlab("GC Content %") + ylab("Tumour Read Counts") + ggtitle("Read count and GC content relationship") + 
@@ -223,23 +175,19 @@ ploidetect_preprocess <- function(all_data, centromeres = F, debugPlots = F, ver
     print(GCnormplot)
   }
   
+  ## Corrected read depth values
   x$residual <- GCnorm$residuals
   
-  ## Detect offset of residual value
-  
+  ## Detect offset of residual value from previous, in case data got shifted
   d <- density(x$residual[maxpeak_ind], n = 2^16)
-  
   offset <- d$x[which.max(d$y)]
   
+  ## Holdover from old code where normalization was applied to bin sizes as well
   x$normalized_size <- x$window_size
   
   
   
-  ## Experimental scaling:
-  
-  #x$fan_correction <- ((x$residual/x$size) * median(x$size)) + median(x$size)
-  
-  
+  ## Another debug plot
   if(debugPlots){
     GCnorm2plot <- ggplot(x, aes(y = residual, x = normalized_size)) + 
       geom_point(size = 0.3, alpha = 0.1) + 
@@ -249,10 +197,10 @@ ploidetect_preprocess <- function(all_data, centromeres = F, debugPlots = F, ver
       ggtitle("Linear normalization of read counts by GC content")
     print(GCnorm2plot)
   }
+  ## Apply corrections to depth, recompute maxpeak
   x <- x %>% mutate(residual = residual + maxpeak - offset) %>% dplyr::rename("corrected_depth" = "residual")
   d <- density(x$corrected_depth, n = 2^16)
   maxpeak <- d$x[which.max(d$y)]
-  #plot(density(x$corrected_depth[x$corrected_depth < 50000])); abline(v = maxpeak)
   output <- list("x" = x, "maxpeak" = maxpeak, "merged" = closest)
   return(output)
 }
